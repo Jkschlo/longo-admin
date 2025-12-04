04b99b748415b1fd435313ed746a2bf00a96415f
Jackson Schlosser <jackson.schlosser1@marist.edu>
2025-12-04 15:15:01 -0500

    updated page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Page() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-50">
-      <h1 className="text-longoBlue text-4xl font-bold">
-        Tailwind is working ✅
-      </h1>
-    </div>
-  );
+  redirect("/login");
 }

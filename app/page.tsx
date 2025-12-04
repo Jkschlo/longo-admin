5a7d2546fd7ad52e72b268875d4d08ce4b27ddbc
Jackson Schlosser <jackson.schlosser1@marist.edu>
2025-12-04 15:51:14 -0500

    test

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 
 export default function Page() {
   const router = useRouter();
+  console.log("Deployment Test!");
 
   useEffect(() => {
     router.replace("/login");
